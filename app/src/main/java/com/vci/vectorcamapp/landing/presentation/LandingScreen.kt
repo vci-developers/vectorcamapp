b149799facd96d676a951512e6453ec415ece2c9
ThomasT0830 <91466914+ThomasT0830@users.noreply.github.com>
2025-06-09 15:14:09 -0400

    SOF-203: Allow scrolling on LandingScreen

--- app/src/main/java/com/vci/vectorcamapp/landing/presentation/LandingScreen.kt
+++ app/src/main/java/com/vci/vectorcamapp/landing/presentation/LandingScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Badge
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -30,7 +31,7 @@ import com.vci.vectorcamapp.ui.theme.VectorcamappTheme
 fun LandingScreen(
     state: LandingState, onAction: (LandingAction) -> Unit, modifier: Modifier = Modifier
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp), // Padding for the overall layout
@@ -38,122 +39,129 @@ fun LandingScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Title Section
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "VectorCam",
-                style = MaterialTheme.typography.displayLarge,
-                color = MaterialTheme.colorScheme.onBackground,
-                fontWeight = FontWeight.ExtraBold,
-            )
-            Spacer(modifier = Modifier.height(16.dp)) // Space below title
-            Text(
-                text = "Democratizing Vector Surveillance",
-                style = MaterialTheme.typography.headlineMedium,
-                color = MaterialTheme.colorScheme.onBackground,
-                textAlign = TextAlign.Center // Align the text content to the center
-            )
+        item {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "VectorCam",
+                    style = MaterialTheme.typography.displayLarge,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.ExtraBold,
+                )
+                Spacer(modifier = Modifier.height(16.dp)) // Space below title
+                Text(
+                    text = "Democratizing Vector Surveillance",
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center // Align the text content to the center
+                )
+            }
         }
 
         // Buttons Section
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(32.dp) // Add spacing between buttons
-        ) {
-            // Surveillance Button
-            Button(
-                onClick = { onAction(LandingAction.StartNewSurveillanceSession) },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .height(60.dp), // Larger height for the button
-                shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
+        item {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(32.dp), // Add spacing between buttons
+                modifier = Modifier.padding(0.dp, 16.dp)
             ) {
-                Text(
-                    text = "New Surveillance Session",
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontWeight = FontWeight.Bold,
-                )
-            }
+                // Surveillance Button
+                Button(
+                    onClick = { onAction(LandingAction.StartNewSurveillanceSession) },
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(60.dp), // Larger height for the button
+                    shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
+                ) {
+                    Text(
+                        text = "New Surveillance Session",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
 
-            // Non-Surveillance Button
-            Button(
-                onClick = { onAction(LandingAction.StartNewNonSurveillanceSession) },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .height(60.dp), // Larger height for the button
-                shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
-            ) {
-                Text(
-                    text = "New Non-Surveillance Session",
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontWeight = FontWeight.Bold,
-                )
-            }
+                // Non-Surveillance Button
+                Button(
+                    onClick = { onAction(LandingAction.StartNewNonSurveillanceSession) },
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(60.dp), // Larger height for the button
+                    shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
+                ) {
+                    Text(
+                        text = "New Non-Surveillance Session",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
 
-            // Incomplete Sessions Button with Badge
-            OutlinedButton(
-                onClick = { onAction(LandingAction.ViewIncompleteSessions) },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .height(60.dp), // Larger height for the button
-                shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = MaterialTheme.colorScheme.secondary // Text color matches border
-                ),
-                border = BorderStroke(
-                    2.dp, MaterialTheme.colorScheme.secondary
-                ) // Custom border
-            ) {
-                Text(
-                    text = "View Incomplete Sessions",
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontWeight = FontWeight.Bold,
-                )
-                Spacer(modifier = Modifier.width(8.dp)) // Space between text and badge
-                if (state.incompleteSessions.isNotEmpty()) {
-                    Badge(
-                        containerColor = MaterialTheme.colorScheme.error,
-                        contentColor = MaterialTheme.colorScheme.onError,
-                        modifier = Modifier.size(24.dp, 24.dp)
-                    ) {
-                        Text(
-                            text = "${state.incompleteSessions.size}",
-                            style = MaterialTheme.typography.bodySmall,
-                            fontWeight = FontWeight.Bold
-                        )
+                // Incomplete Sessions Button with Badge
+                OutlinedButton(
+                    onClick = { onAction(LandingAction.ViewIncompleteSessions) },
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(60.dp), // Larger height for the button
+                    shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        contentColor = MaterialTheme.colorScheme.secondary // Text color matches border
+                    ),
+                    border = BorderStroke(
+                        2.dp, MaterialTheme.colorScheme.secondary
+                    ) // Custom border
+                ) {
+                    Text(
+                        text = "View Incomplete Sessions",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    Spacer(modifier = Modifier.width(8.dp)) // Space between text and badge
+                    if (state.incompleteSessions.isNotEmpty()) {
+                        Badge(
+                            containerColor = MaterialTheme.colorScheme.error,
+                            contentColor = MaterialTheme.colorScheme.onError,
+                            modifier = Modifier.size(24.dp, 24.dp)
+                        ) {
+                            Text(
+                                text = "${state.incompleteSessions.size}",
+                                style = MaterialTheme.typography.bodySmall,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
                     }
                 }
-            }
 
-            // Complete Sessions Button
-            OutlinedButton(
-                onClick = { onAction(LandingAction.ViewCompleteSessions) },
-                modifier = Modifier
-                    .fillMaxWidth(0.8f)
-                    .height(60.dp), // Larger height for the button
-                shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = MaterialTheme.colorScheme.secondary // Text color matches border
-                ),
-                border = BorderStroke(
-                    2.dp, MaterialTheme.colorScheme.secondary
-                ) // Custom border
-            ) {
-                Text(
-                    text = "View Complete Sessions",
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontWeight = FontWeight.Bold,
-                )
+                // Complete Sessions Button
+                OutlinedButton(
+                    onClick = { onAction(LandingAction.ViewCompleteSessions) },
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(60.dp), // Larger height for the button
+                    shape = MaterialTheme.shapes.medium, // Rounded rectangle shape
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        contentColor = MaterialTheme.colorScheme.secondary // Text color matches border
+                    ),
+                    border = BorderStroke(
+                        2.dp, MaterialTheme.colorScheme.secondary
+                    ) // Custom border
+                ) {
+                    Text(
+                        text = "View Complete Sessions",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
             }
         }
 
         // Footer Section
-        Text(
-            text = "Version: ${state.versionName}",
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onBackground,
-        )
+        item {
+            Text(
+                text = "Version: ${state.versionName}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+        }
     }
 }
 
