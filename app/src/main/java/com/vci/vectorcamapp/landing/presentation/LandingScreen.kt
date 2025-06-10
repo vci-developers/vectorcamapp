9f648852268804754fbdaa9dab4527f6ee7f1b95
ThomasT0830 <91466914+ThomasT0830@users.noreply.github.com>
2025-06-10 01:04:49 -0400

    SOF-203: Replaced wildcard imports in LandingScreen.kt

--- app/src/main/java/com/vci/vectorcamapp/landing/presentation/LandingScreen.kt
+++ app/src/main/java/com/vci/vectorcamapp/landing/presentation/LandingScreen.kt
@@ -1,9 +1,23 @@
 package com.vci.vectorcamapp.landing.presentation
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.*
+import androidx.compose.material3.Badge
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
