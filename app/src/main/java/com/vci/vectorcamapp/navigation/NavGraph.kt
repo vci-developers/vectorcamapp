bfb70ea4d6d2d7c6df2d8e69c0ef4c6171c0e5f8
Johnny <shenjiaming2003@gmail.com>
2025-06-16 16:51:04 -0400

    SOF-184: Removed dead code SplashViewModel

--- app/src/main/java/com/vci/vectorcamapp/navigation/NavGraph.kt
+++ app/src/main/java/com/vci/vectorcamapp/navigation/NavGraph.kt
@@ -38,29 +38,6 @@ fun NavGraph() {
     NavHost(
         navController = navController, startDestination = Destination.Landing
     ) {
-        composable<Destination.Splash> {
-            val viewModel = hiltViewModel<SplashViewModel>()
-            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-            LaunchedEffect(uiState.checked) {
-                if (!uiState.checked) return@LaunchedEffect
-
-                if (uiState.session != null) {
-                    navController.navigate(Destination.SurveillanceForm) {
-                        popUpTo(Destination.Splash) { inclusive = true }
-                    }
-                } else {
-                    navController.navigate(Destination.Landing) {
-                        popUpTo(Destination.Splash) { inclusive = true }
-                    }
-                }
-            }
-
-            LoadingAnimation(
-                text = "Loading...", modifier = Modifier.fillMaxSize().padding(16.dp)
-            )
-        }
-
         composable<Destination.Landing> {
             val viewModel = hiltViewModel<LandingViewModel>()
             val state by viewModel.state.collectAsStateWithLifecycle()
