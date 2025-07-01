c19404866926e9193c0590ea24927cdd86342f03
Johnny <shenjiaming2003@gmail.com>
2025-07-01 11:07:43 -0400

    SOF-130: Auto focus at center of bounding box enabled.

--- app/src/main/java/com/vci/vectorcamapp/imaging/presentation/components/camera/LiveCameraPreviewPage.kt
+++ app/src/main/java/com/vci/vectorcamapp/imaging/presentation/components/camera/LiveCameraPreviewPage.kt
@@ -1,5 +1,6 @@
 package com.vci.vectorcamapp.imaging.presentation.components.camera
 
+import androidx.camera.core.FocusMeteringAction
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
@@ -12,8 +13,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -30,19 +34,42 @@ fun LiveCameraPreviewPage(
     onSubmitSession: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
+    val previewView = remember { PreviewView(context) }
+
+    LaunchedEffect(Unit) {
+        previewView.apply {
+            scaleType = PreviewView.ScaleType.FIT_CENTER
+            this.controller = controller
+        }
+        controller.bindToLifecycle(lifecycleOwner)
+    }
+
+    LaunchedEffect(boundingBoxesUiList) {
+        if (boundingBoxesUiList.isNotEmpty()) {
+            val box = boundingBoxesUiList.first()
+            val centerX = box.topLeftX + box.width / 2f
+            val centerY = box.topLeftY + box.height / 2f
+            val point = previewView.meteringPointFactory.createPoint(centerX, centerY)
+
+            val action = FocusMeteringAction
+                .Builder(point, FocusMeteringAction.FLAG_AF)
+                .disableAutoCancel()
+                .build()
+
+            controller.cameraControl?.startFocusAndMetering(action)
+        } else {
+            controller.cameraControl?.cancelFocusAndMetering()
+        }
+    }
+
 
     Box(
         modifier = modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter
     ) {
         AndroidView(
-            factory = { context ->
-                PreviewView(context).apply {
-                    this.controller = controller
-                    scaleType = PreviewView.ScaleType.FIT_CENTER
-                    controller.bindToLifecycle(lifecycleOwner)
-                }
-            },
+            factory = { previewView },
             modifier = Modifier.fillMaxSize()
         )
 
