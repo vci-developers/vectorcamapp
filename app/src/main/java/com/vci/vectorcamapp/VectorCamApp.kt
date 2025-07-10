09944436aecc04ac0a6a8cdf98181614129bb683
Aryaman Shodhan <ashodha1@jhu.edu>
2025-07-10 03:41:07 -0400

    SOF-259: Implement Posthog config

--- app/src/main/java/com/vci/vectorcamapp/VectorCamApp.kt
+++ app/src/main/java/com/vci/vectorcamapp/VectorCamApp.kt
@@ -3,6 +3,8 @@ package com.vci.vectorcamapp
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.posthog.android.PostHogAndroid
+import com.posthog.android.PostHogAndroidConfig
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -16,4 +18,25 @@ class VectorCamApp : Application(), Configuration.Provider {
         get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val postHogConfig = PostHogAndroidConfig(
+            apiKey = BuildConfig.POSTHOG_API_KEY,
+            host = BuildConfig.POSTHOG_HOST
+        )
+
+        postHogConfig.captureApplicationLifecycleEvents = true
+
+        postHogConfig.sessionReplay = true
+
+        postHogConfig.sessionReplayConfig.maskAllImages = false
+        postHogConfig.sessionReplayConfig.maskAllTextInputs = false
+        postHogConfig.sessionReplayConfig.screenshot = true
+
+        postHogConfig.optOut = BuildConfig.DEBUG
+
+        PostHogAndroid.setup(this, postHogConfig)
+    }
 }
