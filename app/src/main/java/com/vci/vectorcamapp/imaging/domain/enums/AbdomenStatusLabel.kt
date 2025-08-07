e7eece9bda05820ff9de3b3b232c2931ccb5b5db
Aryaman Shodhan <ashodha1@jhu.edu>
2025-08-06 22:33:26 -0400

    SOF-268: Update Species, Sex, Abdomen Status models

--- app/src/main/java/com/vci/vectorcamapp/imaging/domain/enums/AbdomenStatusLabel.kt
+++ app/src/main/java/com/vci/vectorcamapp/imaging/domain/enums/AbdomenStatusLabel.kt
@@ -2,6 +2,5 @@ package com.vci.vectorcamapp.imaging.domain.enums
 
 enum class AbdomenStatusLabel(val label: String) {
     UNFED("Unfed"),
-    FULLY_FED("Fully Fed"),
-    GRAVID("Gravid")
+    FULLY_FED("Fully Fed")
 }
