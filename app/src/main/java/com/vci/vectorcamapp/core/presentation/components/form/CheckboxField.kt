318ceb98359bf85d6635205f9644ae74bfdec892
Aryaman Shodhan <ashodha1@jhu.edu>
2025-08-22 19:39:54 -0400

    SOF-303: Remove dead imports

--- app/src/main/java/com/vci/vectorcamapp/core/presentation/components/form/CheckboxField.kt
+++ app/src/main/java/com/vci/vectorcamapp/core/presentation/components/form/CheckboxField.kt
@@ -3,18 +3,13 @@ package com.vci.vectorcamapp.core.presentation.components.form
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role.Companion.Checkbox
 import com.vci.vectorcamapp.ui.extensions.colors
 import com.vci.vectorcamapp.ui.extensions.dimensions
 
