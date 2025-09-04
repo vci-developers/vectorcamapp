a3a0e3318cbbe5587517f3e923e122ae488e3a68
Johnny <105867207+JohnnieShen@users.noreply.github.com>
2025-09-03 22:28:49 -0400

    SOF-305: Search and filter specimens within an incomplete/complete session (#94)
    * SOF-305: First iteration of searching for specimen in complete session details
    
    * SOF-305: Implement comma and space separated query
    
    * SOF-305; Add missing empty line at the end of search utils
    
    * SOF-305: Code refactor, put search utility functions into its own object
    
    * SOF-305: Code refactored
    
    * SOF-305: Update to using our TextEntry Field
    
    * SOF-305: Remove usage for SpecimenImageTile class
    
    * SOF-305: Update variable naming
    
    * SOF-305: Update to performing query on every input value change
    
    * SOF-305: Eliminate need for separate filtered list in state
    
    * SOF-305: Remove magic number, variable rename
    
    * SOF-305: Rename variable
    
    * SOF-305: Code cleanup
    
    * SOF-305L Optimize code

--- app/src/main/java/com/vci/vectorcamapp/complete_session/details/presentation/components/specimens/CompleteSessionSpecimens.kt
+++ app/src/main/java/com/vci/vectorcamapp/complete_session/details/presentation/components/specimens/CompleteSessionSpecimens.kt
@@ -1,55 +1,97 @@
 package com.vci.vectorcamapp.complete_session.details.presentation.components.specimens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.vci.vectorcamapp.core.domain.model.Session
 import com.vci.vectorcamapp.core.domain.model.composites.SpecimenWithSpecimenImagesAndInferenceResults
+import com.vci.vectorcamapp.core.presentation.components.form.TextEntryField
 import com.vci.vectorcamapp.ui.extensions.colors
+import com.vci.vectorcamapp.ui.extensions.dimensions
 import com.vci.vectorcamapp.ui.theme.screenWidthFraction
 
 @Composable
 fun CompleteSessionSpecimens(
     session: Session,
     specimensWithImagesAndInferenceResults: List<SpecimenWithSpecimenImagesAndInferenceResults>,
+    searchQuery: String,
+    onUpdateSearchQuery: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    if (specimensWithImagesAndInferenceResults.isEmpty()) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    if (specimensWithImagesAndInferenceResults.isEmpty() && searchQuery.isBlank()) {
         Text(
             "No specimens were captured during this session.",
             style = MaterialTheme.typography.headlineSmall,
             color = MaterialTheme.colors.textSecondary,
             textAlign = TextAlign.Center,
-            modifier = modifier.fillMaxSize()
+            modifier = Modifier.padding(MaterialTheme.dimensions.paddingMedium)
         )
     } else {
-        LazyRow(
-            modifier = modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center
+        Column(
+            modifier = modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            items(items = specimensWithImagesAndInferenceResults.asReversed()) { specimenWithSpecimenImagesAndInferenceResults ->
-                val specimen = specimenWithSpecimenImagesAndInferenceResults.specimen
-                val imageList =
-                    specimenWithSpecimenImagesAndInferenceResults.specimenImagesAndInferenceResults
-                val totalImages = imageList.size
+            TextEntryField(
+                value = searchQuery,
+                onValueChange = { newSearchQuery -> onUpdateSearchQuery(newSearchQuery) },
+                placeholder = "Search by specimen ID, species, etc.",
+                modifier = Modifier.padding(horizontal = MaterialTheme.dimensions.spacingMedium),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        keyboardController?.hide()
+                    }
+                )
+            )
 
-                imageList.mapIndexed { index, (specimenImage, _) ->
-                    CompleteSessionSpecimensTile(
-                        session = session,
-                        specimen = specimen,
-                        specimenImage = specimenImage,
-                        badgeText = "${index + 1} of $totalImages",
-                        modifier = Modifier.width(
-                            screenWidthFraction(0.9f)
-                        )
-                    )
+            if (specimensWithImagesAndInferenceResults.isEmpty()) {
+                Text(
+                    text = "No matching specimens found.",
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colors.textSecondary,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(MaterialTheme.dimensions.paddingMedium)
+                )
+            } else {
+                LazyRow(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    items(items = specimensWithImagesAndInferenceResults.asReversed()) { specimenWithSpecimenImagesAndInferenceResults ->
+                        val specimen = specimenWithSpecimenImagesAndInferenceResults.specimen
+                        val imageList =
+                            specimenWithSpecimenImagesAndInferenceResults.specimenImagesAndInferenceResults
+                        val totalImages = imageList.size
+                        imageList.mapIndexed { index, (specimenImage, _) ->
+                            CompleteSessionSpecimensTile(
+                                session = session,
+                                specimen = specimen,
+                                specimenImage = specimenImage,
+                                badgeText = "${index + 1} of $totalImages",
+                                modifier = Modifier.width(
+                                    screenWidthFraction(0.9f)
+                                )
+                            )
+                        }
+                    }
                 }
             }
         }
