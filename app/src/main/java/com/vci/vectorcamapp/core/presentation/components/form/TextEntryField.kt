94f1b983274b31458993d9edf227091bf04d8ba2
Aryaman Shodhan <ashodha1@jhu.edu>
2025-09-29 23:06:34 -0400

    SOF-349: Limit data entry inside text entry fields to characters and digits

--- app/src/main/java/com/vci/vectorcamapp/core/presentation/components/form/TextEntryField.kt
+++ app/src/main/java/com/vci/vectorcamapp/core/presentation/components/form/TextEntryField.kt
@@ -33,6 +33,7 @@ fun TextEntryField(
     placeholder: String? = null,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
+    maxCharacters: Int = 200,
 ) {
     val context = LocalContext.current
 
@@ -50,7 +51,11 @@ fun TextEntryField(
 
         OutlinedTextField(
             value = value,
-            onValueChange = onValueChange,
+            onValueChange = { newValue ->
+                if (newValue.all { it.isLetterOrDigit() } && newValue.length <= maxCharacters) {
+                    onValueChange(newValue)
+                }
+            },
             isError = error != null,
             singleLine = singleLine,
             keyboardOptions = keyboardOptions,
