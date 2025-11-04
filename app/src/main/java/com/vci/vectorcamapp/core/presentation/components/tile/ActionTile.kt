78d73a2bbc74ee5ee541c3d5101f4c268c11e5fa
Thomas Tseng <91466914+ThomasT0830@users.noreply.github.com>
2025-11-04 03:47:34 -0500

    SOF-352: Give CompleteSessionListTile a background hue based on progress (#127)
    * SOF-352: Give tiles a hue
    
    * SOF-352: Update CompleteSessionListTile shadow
    
    * SOF-352: Simplify ActionTile
    
    * SOF-352: Set opacity in ActionTile

--- app/src/main/java/com/vci/vectorcamapp/core/presentation/components/tile/ActionTile.kt
+++ app/src/main/java/com/vci/vectorcamapp/core/presentation/components/tile/ActionTile.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.vci.vectorcamapp.ui.extensions.colors
 import com.vci.vectorcamapp.ui.extensions.customShadow
 import com.vci.vectorcamapp.ui.extensions.dimensions
@@ -16,6 +17,7 @@ import com.vci.vectorcamapp.ui.extensions.dimensions
 fun ActionTile(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    hue: Color = MaterialTheme.colors.cardGlow,
     content: @Composable () -> Unit
 ) {
     Card(
@@ -29,7 +31,7 @@ fun ActionTile(
                 vertical = MaterialTheme.dimensions.paddingSmall
             )
             .customShadow(
-                color = MaterialTheme.colors.cardGlow.copy(alpha = 0.2f),
+                color = hue.copy(alpha = 0.25f),
                 offsetX = MaterialTheme.dimensions.shadowOffsetSmall,
                 offsetY = MaterialTheme.dimensions.shadowOffsetSmall,
                 cornerRadius = MaterialTheme.dimensions.cornerRadiusMedium,
