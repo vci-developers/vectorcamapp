72f36fc9ac0e7fb025a5751992a28329a8bd183c
Stephen Adipradhana <stephenadipradhana@gmail.com>
2026-02-14 12:45:54 +0700

    fix: rename class

--- app/src/main/java/com/vci/vectorcamapp/core/di/PresentationModule.kt
+++ app/src/main/java/com/vci/vectorcamapp/core/di/PresentationModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class PresentationModule {
+abstract class ErrorMessageModule {
 
     @Binds
     @Singleton
