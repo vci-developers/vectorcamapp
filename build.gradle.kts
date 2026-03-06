bb5690c5bf36a474a6bfb1041d28d21a2ca57e2c
Stephen Adipradhana <stephenadipradhana@gmail.com>
2026-03-06 21:22:49 +0700

    feat: add detekt step on ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,23 @@ plugins {
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${libs.versions.detekt.get()}")
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom(files("${rootDir}/config/detekt/detekt.yml"))
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = "11"
 }
