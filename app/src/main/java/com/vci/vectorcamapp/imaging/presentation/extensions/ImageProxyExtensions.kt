7164963997018121079db4612eb80823536bcf60
Stephen Adipradhana <stephenadipradhana@gmail.com>
2026-03-17 14:48:50 +0700

    fix: warning

--- app/src/main/java/com/vci/vectorcamapp/imaging/presentation/extensions/ImageProxyExtensions.kt
+++ app/src/main/java/com/vci/vectorcamapp/imaging/presentation/extensions/ImageProxyExtensions.kt
@@ -2,6 +2,7 @@ package com.vci.vectorcamapp.imaging.presentation.extensions
 
 import android.graphics.Bitmap
 import androidx.camera.core.ImageProxy
+import androidx.core.graphics.createBitmap
 import org.opencv.android.Utils.bitmapToMat
 import org.opencv.android.Utils.matToBitmap
 import org.opencv.core.Core
@@ -18,7 +19,7 @@ fun ImageProxy.toUprightBitmap(): Bitmap {
     Core.rotate(src, dst, Core.ROTATE_90_CLOCKWISE)
     src.release()
 
-    val result = Bitmap.createBitmap(dst.cols(), dst.rows(), Bitmap.Config.ARGB_8888)
+    val result = createBitmap(dst.cols(), dst.rows())
     matToBitmap(dst, result)
     dst.release()
 
