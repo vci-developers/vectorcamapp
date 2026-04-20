c367b374fa2ac53ef82e1cc8edf103d222be3079
Stephen Adipradhana <stephenadipradhana@gmail.com>
2026-04-20 11:15:50 +0700

    fix: image rotation on some phone

--- app/src/main/java/com/vci/vectorcamapp/imaging/presentation/extensions/ImageProxyExtensions.kt
+++ app/src/main/java/com/vci/vectorcamapp/imaging/presentation/extensions/ImageProxyExtensions.kt
@@ -9,14 +9,30 @@ import org.opencv.core.Core
 import org.opencv.core.Mat
 
 fun ImageProxy.toUprightBitmap(): Bitmap {
-    val rawBitmap = this.toBitmap()
+    val rawBitmap = toBitmap()
+    val rotationDegrees = ((imageInfo.rotationDegrees % 360) + 360) % 360
+
+    if (rotationDegrees == 0) {
+        return rawBitmap
+    }
+
+    val rotateCode = when (rotationDegrees) {
+        90 -> Core.ROTATE_90_CLOCKWISE
+        180 -> Core.ROTATE_180
+        270 -> Core.ROTATE_90_COUNTERCLOCKWISE
+        else -> null
+    }
+
+    if (rotateCode == null) {
+        return rawBitmap
+    }
 
     val src = Mat()
     bitmapToMat(rawBitmap, src)
     rawBitmap.recycle()
 
     val dst = Mat()
-    Core.rotate(src, dst, Core.ROTATE_90_CLOCKWISE)
+    Core.rotate(src, dst, rotateCode)
     src.release()
 
     val result = createBitmap(dst.cols(), dst.rows())
