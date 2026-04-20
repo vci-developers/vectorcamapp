293203c0982162a691f18cd85e6d6ec4e0e0d546
Stephen Adipradhana <stephenadipradhana@gmail.com>
2026-04-20 12:27:35 +0700

    fix: passsword

--- app/src/main/java/com/vci/vectorcamapp/registration/domain/constants/ProgramRegistrationPasswords.kt
+++ app/src/main/java/com/vci/vectorcamapp/registration/domain/constants/ProgramRegistrationPasswords.kt
@@ -3,17 +3,17 @@ package com.vci.vectorcamapp.registration.domain.constants
 object ProgramRegistrationPasswords {
     private val passwordsByProgramId = mapOf(
         // Production (api.vectorcam.org)
-        1 to "nmed-uganda",    // National Malaria Elimination Division – Uganda
-        2 to "data-colombia",  // Data Collection – Colombia
-        3 to "pilot-ghana",    // Pilot Field Study – Ghana
-        4 to "kemri-kenya",    // KEMRI - AnoSTEP – Kenya
+        1 to "nmedug01",       // National Malaria Elimination Division – Uganda
+        2 to "datacol2",       // Data Collection – Colombia
+        3 to "pilogh03",       // Pilot Field Study – Ghana
+        4 to "kemriken4",      // KEMRI - AnoSTEP – Kenya
 
         // Staging (test.api.vectorcam.org)
-        7 to "nmed-uganda",    // National Malaria Elimination Division – Uganda
-        8 to "test-1234",      // TEST
-        9 to "pilot-ghana",    // Pilot Field Study – Ghana
-        10 to "anostep-kenya", // Kenya ANOSTEP – Kenya
-        11 to "test2-1234",    // TEST 2
+        7 to "nmedug07",       // National Malaria Elimination Division – Uganda
+        8 to "testprog8",      // TEST
+        9 to "pilogh09",       // Pilot Field Study – Ghana
+        10 to "anokeny10",     // Kenya ANOSTEP – Kenya
+        11 to "testpro11",     // TEST 2
     )
 
     fun passwordForProgram(programId: Int): String? = passwordsByProgramId[programId]
