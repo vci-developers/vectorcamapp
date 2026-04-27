863bda8afa46cc45a811cfdfa647293cf4fc0282
Stephen Adipradhana <stephenadipradhana@gmail.com>
2026-04-27 08:31:53 +0700

    fix: remove comment

--- app/src/main/java/com/vci/vectorcamapp/core/data/upload/metadata/MetadataUploadWorker.kt
+++ app/src/main/java/com/vci/vectorcamapp/core/data/upload/metadata/MetadataUploadWorker.kt
@@ -387,7 +387,6 @@ class MetadataUploadWorker @AssistedInject constructor(
                 latitude = remoteSessionDto.latitude,
                 longitude = remoteSessionDto.longitude,
                 type = remoteSessionDto.type,
-                // todo stephen add collection cycle here
             )
 
             if (localSessionDto != remoteSessionDto) {
